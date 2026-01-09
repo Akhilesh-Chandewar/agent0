1d3fe32aed5c962d57ca067615611e6c787d904b
Akhil Chandewar <akhil.chandewar00@gmail.com>
2026-01-09 18:13:28 +0530

    clerk auth and momgoose connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-export default function Home() {
+import connectToDatabase from "@/lib/databaseConnection";
+
+export default async function Home() {
+  await connectToDatabase();
   return (
     <>
     </>
